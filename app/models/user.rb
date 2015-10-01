bf043bc1de1185ac83c8d27f123526881d9f0f2a
robrieba <robrieba@gmail.com>
2015-10-01 19:50:28 -0400

    added devise

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+def self.gravatar_url(user, size)
+  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
+end
+
+
+end
