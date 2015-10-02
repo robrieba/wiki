46a4a568a4b440f32f0dfcd42ee4216eb6ddd51d
robrieba <robrieba@gmail.com>
2015-10-02 19:13:55 -0400

    added production mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-def self.gravatar_url(user, size)
-  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-  "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
-end
-
+  def self.gravatar_url(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
+  end
 
 end
